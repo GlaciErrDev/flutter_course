aad21722f175a6dfcb815a18440df28b3d678a77
Mike Durnosvystov <mike.durnosvystov@nextiva.com>
2019-01-29 19:07:59 +0200

    Initial commit
    module - Diving Into the Basics & Understanding Widgets
    stop point - Passing Data to Stateful Widgets

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import './product_manager.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('List of products'),
+        ),
+        body: ProductManager('Nissan 4x4'),
+      ),
+    );
+  }
+}
